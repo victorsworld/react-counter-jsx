614caabf84df8df2660668104a63576fae3cb052
victorsworld <victordirect48@gmail.com>
2023-09-24 12:44:56 -0400

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [star, setStar] = useState([]);
 
+  const minus = () => {
+    setCount(count - 1);
+    let a = [...star];
+    a.pop();
+    setStar(a);
+  };
+
+  const add = () => {
+    setCount(count + 1);
+
+    if (count >= 0) {
+      let a = [...star];
+      a.push('*');
+      setStar(a);
+    }
+  };
+
+  console.log(star);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div id='container'>
+      <nav >STARS</nav>
+      <div >
+        <button  onClick={minus}>
+          Remove
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <span >{count}</span>
+        <button  onClick={add}>
+          Add
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div id="star">
+        <span>{star}</span>
+      </div>
+    </div>
+  );
 }
-
-export default App
+export default App;
